7702cf9b5857bd39a17510cf6a24cad2c7d9a975
fhinnn <deltras12388@gmail.com>
2024-07-29 01:35:23 +0700

    feat: add monitor user proxysql

--- Master-Master/proxy.sh
+++ Master-Master/proxy.sh
@@ -18,10 +18,20 @@ proxy_stmt_2="INSERT INTO mysql_users (username, password, default_hostgroup) VA
 
 proxy_stmt_3="INSERT INTO mysql_query_rules (rule_id, active, username, match_digest, destination_hostgroup, apply) VALUES (1,1,'$PROXY_USER','^select',2,1);"
 
+proxy_stmt_4="set mysql-server_version='8.0.26';
+LOAD MYSQL VARIABLES TO RUNTIME;
+SAVE MYSQL VARIABLES TO DISK;
+SET admin-web_enabled='true';
+LOAD ADMIN VARIABLES TO RUNTIME;
+SAVE ADMIN VARIABLES TO DISK;"
+
+
 # SET VARIABLES
 docker exec proxysql-server sh -c "mysql -u $PROXY_ADMIN_USER -p$PROXY_ADMIN_PASSWORD -P$PROXY_ADMIN_PORT -e \"$proxy_stmt_1\"" && echo "Hostgroup Configured"
 docker exec proxysql-server sh -c "mysql -u $PROXY_ADMIN_USER -p$PROXY_ADMIN_PASSWORD -P$PROXY_ADMIN_PORT -e \"$proxy_stmt_2\"" && echo "User Configured"
 docker exec proxysql-server sh -c "mysql -u $PROXY_ADMIN_USER -p$PROXY_ADMIN_PASSWORD -P$PROXY_ADMIN_PORT -e \"$proxy_stmt_3\"" && echo "Query Rule Configured"
+docker exec proxysql-server sh -c "mysql -u $PROXY_ADMIN_USER -p$PROXY_ADMIN_PASSWORD -P$PROXY_ADMIN_PORT -e \"$proxy_stmt_4\"" && echo "Mysql Version & Monitor Configured"
+
 
 # Save Changes
 proxy_stmt_4="LOAD MYSQL SERVERS TO RUNTIME;
@@ -36,6 +46,10 @@ docker exec proxysql-server sh -c "mysql -u $PROXY_ADMIN_USER -p$PROXY_ADMIN_PAS
 # Proxy Apply
 proxy_stmt_5="CREATE USER '$PROXY_USER'@'%' IDENTIFIED BY '$PROXY_PASSWORD'; 
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$PROXY_USER'@'%';
+FLUSH PRIVILEGES;
+CREATE USER 'monitor'@'%' IDENTIFIED BY 'monitor';
+GRANT SELECT ON *.* TO 'monitor'@'%';
 FLUSH PRIVILEGES;"
+
 
 docker exec mysql_master-1 sh -c "mysql -u root -e \"$proxy_stmt_5\"" && echo "Proxy Connected"
